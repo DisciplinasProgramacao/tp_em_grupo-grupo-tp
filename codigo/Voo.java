8c6719b44d910f3745e195d598483836be420783
Gabriel Oliveira Peron <114201395+GabrielOlPeron@users.noreply.github.com>
2022-10-24 15:25:35 -0300

    Update Voo.java

--- codigo/Voo.java
+++ codigo/Voo.java
@@ -1,14 +1,20 @@
 public class Voo {
+    private static double VALOR_BASE;
     private Data data;
     private Trecho trecho;
 
     /**
      * Método construtor
      */
-    public Voo(Data data, Trecho trecho){
+    public Voo(Data data, Trecho trecho, double valorBase){
         /**Adicionar verificação de valor valido */
         this.data = data;
         this.trecho = trecho;
+        this.VALOR_BASE = valorBase
+    }
+    
+    public void AlterarTrecho(String novaOrigem, String novoDestino){
+
     }
 
     public String informacoesDoVoo(){
