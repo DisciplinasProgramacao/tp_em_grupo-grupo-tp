fcc50d5435c4636478e18c17a10225c91f262034
Gabriel Oliveira Peron <114201395+GabrielOlPeron@users.noreply.github.com>
2022-11-19 20:44:52 -0300

    Update Voo.java

--- codigo/Voo.java
+++ codigo/Voo.java
@@ -1,31 +1,56 @@
+
 public class Voo {
+    // #region atributos
     private double valorBase;
     private Data data;
     private Trecho trecho;
 
+    // #endregion
+
     /**
      * Método construtor
+     * 
+     * @param data      Data de partida do voo
+     * @param trecho    Trecho percorrido pelo voo
+     * @param valorBase Valor base do voo
      */
-    public Voo(Data data, Trecho trecho, double valorBase){
-        /**Adicionar verificação de valor valido */
+    public Voo(Data data, Trecho trecho, double valorBase) {
+
         this.data = data;
         this.trecho = trecho;
-        this.valorBase=valorBase;
-    }
-    
-    public void AlterarTrecho(String novaOrigem, String novoDestino){
-
+        this.valorBase = valorBase;
     }
 
-    public String informacoesDoVoo(){
-        return null;
+    /**
+     * Altera o trecho do voo
+     * 
+     * @param numero      Número do trecho
+     * @param novaOrigem  Aeroporto de origem
+     * @param novoDestino Aeroporto de destino
+     */
+    public void AlterarTrecho(int numero, String novaOrigem, String novoDestino) {
+        this.trecho = new Trecho(numero, novaOrigem, novoDestino);
     }
 
-	public double getValorBase() {
-		return valorBase;
-	}
+    /**
+     * Retorna a descrição do voo. Formato:
+     * <numero>-<origem> para <destino> em <data>.
+     * valor base-<valorBase>
+     * 
+     * @return String no formato indicado.
+     */
+    public String informacoesDoVoo() {
+        StringBuilder informacoes = new StringBuilder("-----\n");
+        informacoes.append(
+                this.trecho.informacoesDoTrecho() + " em " + data.dataFormatada() + "\n Valor base-" + valorBase);
+        return informacoes.toString();
+    }
 
-	public void setValorBase(double valorBase) {
-		this.valorBase = valorBase;
-	}
+    public double getValorBase() {
+        return valorBase;
+    }
+
+    public void setValorBase(double valorBase) {
+        this.valorBase = valorBase;
+    }
 }
