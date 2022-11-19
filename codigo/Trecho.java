c6e221ff2ebe757f70b9a125f81113daab58b608
Gabriel Oliveira Peron <114201395+GabrielOlPeron@users.noreply.github.com>
2022-11-19 18:00:41 -0300

    Update Trecho.java

--- codigo/Trecho.java
+++ codigo/Trecho.java
@@ -1,20 +1,34 @@
+/**
+ * Classe Trecho
+ */
 public class Trecho {
+
+    // #region atributos
     private int numero;
     private String origem;
     private String destino;
 
+    // #endregion
+
     /**
      * Método construtor
      */
-    public Trecho(int numero, String origem, String destino){
-        /**Adicionar verificacao de valor valido */
+    public Trecho(int numero, String origem, String destino) {
+
         this.numero = numero;
         this.origem = origem;
         this.destino = destino;
-
     }
 
-    public String informacoesDoTrecho(){
-        return null;
+    /**
+     * Retorna a descrição do trecho. Formato:
+     * Origem <origem>, destino <destino>, número do Trecho <numero>
+     * 
+     * @return String no formato indicado.
+     */
+    public String informacoesDoTrecho() {
+        StringBuilder descricao = new StringBuilder("-----\n");
+        descricao.append("Origem " + this.origem + ", destino " + this.destino + ", número do trecho " + numero);
+        return descricao.toString();
     }
 }
